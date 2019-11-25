dac00ca0af5ed38843c495ba6803d44ba066f434
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-25 20:20:06 +0900

    랜덤넘버 6개를 1~45, 중복 검사를 해서 집어넣고 출력

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
@@ -2,9 +2,13 @@ package com.tjoeun.a201911_kotlinfinaltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_lotto.*
 
 class LottoActivity : BaseActivity() {
 
+    var lottoArrayList = ArrayList<Int>()
+    var lottoTextArrayList = ArrayList<TextView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,10 +18,44 @@ class LottoActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        lottoBtnBuyOne.setOnClickListener {
+            setThisWeekLottoNum()
 
+        }
+    }
+
+    //숫자를 랜덤으로 6개를 생성 1 ~ 45, 중복이 되면 안됨.
+    fun setThisWeekLottoNum(){
+        for(lotTxt in lottoTextArrayList){
+
+            var randomNum = 0
+
+                while (true) {
+                    randomNum = (Math.random() * 45 + 1).toInt()
+
+                    var isDupOk = true
+
+                    for(num in lottoArrayList){
+                        if(num == randomNum){
+                            isDupOk = false
+                            break
+                        }
+                    }
+                    if(isDupOk){
+                        lottoArrayList.add(randomNum)
+                        break
+                    }
+                }
+            lotTxt.text = randomNum.toString()
+        }
     }
 
     override fun setValues() {
-
+        lottoTextArrayList.add(lottoTxtLotNum1)
+        lottoTextArrayList.add(lottoTxtLotNum2)
+        lottoTextArrayList.add(lottoTxtLotNum3)
+        lottoTextArrayList.add(lottoTxtLotNum4)
+        lottoTextArrayList.add(lottoTxtLotNum5)
+        lottoTextArrayList.add(lottoTxtLotNum6)
     }
 }
