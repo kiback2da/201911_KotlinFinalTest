2baaca54a50e4cbda67131b1ca1223f5cb923734
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-25 19:55:35 +0900

    메인화면에서 백버튼을 눌렀을때 토스트 띄우기

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/MainActivity.kt
@@ -3,9 +3,12 @@ package com.tjoeun.a201911_kotlinfinaltest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    var lastBackButtonPressedTime = 0L
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,22 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+    }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+
+        val currentTime = System.currentTimeMillis() //현재 시간을 간편하게 따고 싶을때 사용
+        if(currentTime - lastBackButtonPressedTime > 1500){
+            //백버튼을 오랜만에 누른 상황
+            Toast.makeText(mContext,"한번 더 뒤로 버튼을 누르면 앱이 종료됩니다.",Toast.LENGTH_SHORT).show()
+
+        }else {
+            finish()
+        }
+
+        //화면이 닫히지 않았다면 눌린 시간을 기록
+        lastBackButtonPressedTime = currentTime
     }
 
     override fun setupEvents() {
